066fc13eabec77d4a99c4fda95177bd8d819cf82
amandhamdo <mramdy213@gmail.com>
2024-09-12 12:08:38 -0300

    Add files via upload

---
+++ recintos-zoo.js
@@ -0,0 +1,23 @@
+class RecintosZoo {
+ RecintosparaAnimais = [
+    {numero: 1, savana, tamanho : 10, animaisexistentes : { MACACO : 3, }},
+    {numero: 2, floresta, tamanho : 5, animaisexistentes : {VAZIO}},
+    {numero: 3, savanaOUrio,tamanho:7, animaisexistentes:{GAZELA: 1} },
+    {numero: 4, rio, tamanho: 8, animaisexistentes: {VAZIO}},
+    {numero: 5, savana, tamanho: 9, animaisexistentes:{LEÃO: 1}}
+ ]
+
+ Animais = [
+    {LEAO, tamanho: 3, savana},
+    {LEOPARDO,tamanho:2,savana},
+    {CROCODILO,tamanho:3,rio},
+    {MACACO, tamanho:1,savanaoufloresta},
+    {GAZELA, tamanho:2, savana},
+    {HIPOPOTAMO,tamanho: 4, savanaOUrio}
+ ]
+    analisaRecintos(animmal, quantidade){
+    }
+
+};
+
+export { RecintosZoo as RecintosZoo };
